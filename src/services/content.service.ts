03baf6acf337bf3f404a7aee5b928e2bf4aa46e7
ThisIsVladis <mail@yourmail.ru>
2023-12-12 01:53:51 +0300

    pre-alfa fix

--- src/services/content.service.ts
+++ src/services/content.service.ts
@@ -1,21 +1,24 @@
 'use server'
 
 import {HotelData} from "@/interfaces/content.interfaces";
-import { headers } from 'next/headers';
-
-const headersList = headers();
-const domain = headersList.get('host') || "";
-
-let API_URL = '';
-
-if (domain === 'https://ivanovohotel.ivhg.ru') {
-  API_URL = 'http://94.241.138.67:8000/api/v1/test/hotel1';
-} else if (domain === 'https://ivanovohotel.ivhg.ru') {
-  API_URL = 'http://94.241.138.67:8000/api/v1/test/hotel2';
-}
-
+import {headers} from "next/headers";
 
 export const getContent = async (): Promise<HotelData> => {
+  const headersList = headers();
+  const domain = headersList.get('host') || "";
+
+  console.log(domain)
+
+  let API_URL = '';
+
+  if (domain === 'localhost:3000') {
+    API_URL = 'http://94.241.138.67:8000/api/v1/test/hotel1';
+  } else if (domain === 'localhost:3000') {
+    API_URL = 'http://94.241.138.67:8000/api/v1/test/hotel2';
+  }
+
+  console.log('Current API_URL:', domain, 'domain');
+
   try {
     const response = await fetch(API_URL, { cache: 'no-store' });
     if (!response.ok) {
