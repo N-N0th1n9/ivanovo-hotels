0b9cfb41fc9b702ac80a370ddf75dbcb48fcd9fb
ThisIsVladis <mail@yourmail.ru>
2023-12-22 22:20:39 +0300

    fix redirect

--- next.config.js
+++ next.config.js
@@ -4,16 +4,25 @@ module.exports = {
       {
         source: '/(.*)',
         destination: 'https://ivanovohotel.ivhg.ru/hotel',
+        has: [
+          { type: 'host', value: 'ivanovohotel.ivhg.ru' },
+        ],
         permanent: true,
       },
       {
         source: '/(.*)',
         destination: 'https://arthotel.ivhg.ru/hotel',
+        has: [
+          { type: 'host', value: 'arthotel.ivhg.ru' },
+        ],
         permanent: true,
       },
       {
         source: '/(.*)',
         destination: 'https://ivhg.ru/hotel-selection',
+        has: [
+          { type: 'host', value: 'ivhg.ru' },
+        ],
         permanent: true,
       },
     ];
