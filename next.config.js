c06c894ab043f42062ff36e02a26f7faee8030c9
ThisIsVladis <mail@yourmail.ru>
2023-12-22 21:35:07 +0300

    fix redirect

--- next.config.js
+++ next.config.js
@@ -3,17 +3,17 @@ module.exports = {
   async redirects() {
     return [
       {
-        source: 'https://ivanovohotel.ivhg.ru/:path*',
+        source: 'https://ivanovohotel.ivhg.ru',
         destination: 'https://ivanovohotel.ivhg.ru/hotel',
         permanent: true,
       },
       {
-        source: 'https://arthotel.ivhg.ru/:path*',
+        source: 'https://arthotel.ivhg.ru',
         destination: 'https://arthotel.ivhg.ru/hotel',
         permanent: true,
       },
       {
-        source: 'https://ivhg.ru/',
+        source: 'https://ivhg.ru',
         destination: 'https://ivhg.ru/hotel-selection',
         permanent: true,
       },
