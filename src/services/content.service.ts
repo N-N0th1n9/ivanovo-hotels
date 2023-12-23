ba33aa4490fc8ef6d2f4e051cd678f4d420592ee
ThisIsVladis <mail@yourmail.ru>
2023-12-23 20:30:04 +0300

    beta ready

--- src/services/content.service.ts
+++ src/services/content.service.ts
@@ -9,17 +9,11 @@ export const getContent = async (subdomain: string): Promise<HotelData> => {
 
   let API_URL = '';
 
-  if (domain === 'localhost:3000') {
-    API_URL = 'http://94.241.138.67:8000/api/v1/test/hotel2';
-  } else if (domain === 'localhost:3000') {
+  if (domain === 'ivanovohotel.ivhg.ru') {
+    API_URL = 'http://94.241.138.67:8000/api/v1/test/hotel1';
+  } else if (domain === 'arthotel.ivhg.ru') {
     API_URL = 'http://94.241.138.67:8000/api/v1/test/hotel2';
   }
-
-  // if (domain === 'ivanovohotel.ivhg.ru') {
-  //   API_URL = 'http://94.241.138.67:8000/api/v1/test/hotel1';
-  // } else if (domain === 'arthotel.ivhg.ru') {
-  //   API_URL = 'http://94.241.138.67:8000/api/v1/test/hotel2';
-  // }
 
   try {
     const response = await fetch(API_URL, { cache: 'no-store' });
